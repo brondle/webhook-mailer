77a398765e7392de9ae54cb10c13b3305f09704f
Brent Bailey <baileyb@alumni.reed.edu>
2016-02-15 20:29:09 -0500

    prune email addresses for testing

--- mailer.js
+++ mailer.js
@@ -81,7 +81,7 @@ function sendMail(JSON) {
        var transporter = nodemailer.createTransport(mg(auth));
          var mailOptions = {
            from: 'brent@the-mealplan.com',
-           to: 'brentlbailey@gmail.com, order@the-mealplan.com, pasang.thinay@the-mealplan.com',
+           to: 'brentlbailey@gmail.com',
            subject: JSON.order_number,
            // text: ,
            html: '<b>Order #' + + JSON.order_number +'</b>',

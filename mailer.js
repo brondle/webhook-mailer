1a164fca9b68f5f1f049bf8d0a5164b6093920df
Brent Bailey <baileyb@alumni.reed.edu>
2016-02-16 22:31:57 -0500

    formatting changes

--- mailer.js
+++ mailer.js
@@ -7,6 +7,7 @@ var mg = require('nodemailer-mailgun-transport');
 var fs = require('fs');
 var idStorage = [];
 app.use(bodyParser.json());
+app.use(express.static('img'));
 
 app.post('/', function(req, res) {
  var JSON = req.body;
@@ -34,31 +35,69 @@ function generatePDF(JSON) {
    var doc = new PDFDocument();
    var writeStream = fs.createWriteStream('output.pdf');
    doc.pipe(writeStream);
-   var lineItems = JSON.line_items;
+   //  Scale proprotionally to the specified width
+
    var address = JSON.shipping_address;
+    var lineItems = JSON.line_items;
+
    for (var x=0; x<lineItems.length; x++) {
     item=lineItems[x];
     if (item.quantity === 1) {
+      doc.image('./logo.jpg', 0, 0);
+      doc.moveDown();
+      doc.text('The Meal Plan', doc.page.width/2 - 40, 200);
+      doc.moveDown();
+      doc.text('New York, NY');
+      doc.moveDown();
+      doc.lineWidth(1);
+      doc.lineCap('square');
+      doc.moveTo(0, 370);
+      doc.lineTo(620, 370);
+      doc.stroke();
+      doc.moveTo(0, 370);
+      doc.moveDown();
+
        //include delivery address, item quantity, title, price and order notes
       addAddress(doc, address);
-      doc.text(item.quantity + " " + item.title + " " + item.price);
+      doc.text(item.title).font('Times-Bold');
       doc.moveDown(1);
       addProperties(doc, item, JSON);
+      doc.moveDown(5);
+      doc.text("Enjoy your meal! - The Meal Plan")
+      doc.font('Times-Roman');
+
       doc.addPage();
 
    } else {
       //make sure each instance of an item gets its own page even if more than one of it was ordered
       for (var i=0; i < item.quantity; i ++) {
+        doc.image('./logo.jpg', 0, 0);
+        doc.moveDown();
+        doc.text('The Meal Plan', doc.page.width/2 - 40, 200);
+        doc.moveDown();
+        doc.text('New York, NY');
+        doc.moveDown();
+        doc.lineWidth(1);
+        doc.lineCap('square');
+        doc.moveTo(0, 370);
+        doc.lineTo(620, 370);
+        doc.stroke();
+        doc.moveTo(0, 370);
+
         addAddress(doc, address);
-        doc.text("1     " + item.title + "       " + item.price);
+        doc.moveDown(5);
+        doc.text(item.title).font('Times-Bold');
         addProperties(doc, item, JSON);
+        doc.text("Enjoy your meal! - The Meal Plan")
+        doc.font('Times-Roman');
+
         doc.addPage();
       }
    }
 
    doc.save();
    writeStream.on('finish', function() {
-       sendMail(JSON);
+       // sendMail(JSON);
     });
   }
    doc.end();
@@ -81,7 +120,7 @@ function sendMail(JSON) {
        var transporter = nodemailer.createTransport(mg(auth));
          var mailOptions = {
            from: 'brent@the-mealplan.com',
-           to: 'brentlbailey@gmail.com, pasang.thinay@the-mealplan.com, order@the-mealplan.com',
+           to: 'brentlbailey@gmail.com',
            subject: JSON.order_number,
            // text: ,
            html: '<b>Order #' + + JSON.order_number +'</b>',
@@ -104,7 +143,15 @@ function sendMail(JSON) {
  }
 
  function addAddress(doc, address) {
-    doc.text(address.first_name + " " + address.last_name);
+    var oldY = doc.y;
+    var addressWidth = (doc.widthOfString(address.city + ", " + address.province + ", " + address.zip)) + 50;
+    doc.lineWidth(1);
+    doc.lineCap('square');
+    doc.moveTo(20, doc.y);
+    doc.lineTo(addressWidth, doc.y);
+    doc.stroke();
+    doc.moveTo(20, doc.y);
+    doc.text(address.first_name + " " + address.last_name, 40);
     doc.moveDown();
     doc.text(address.phone);
     doc.moveDown();
@@ -115,10 +162,28 @@ function sendMail(JSON) {
   }
     doc.moveDown();
     doc.text(address.city + ", " + address.province + ", " + address.zip);
+    doc.moveTo(20, doc.y);
+    doc.lineTo(20, oldY);
+    doc.stroke();
+    doc.moveTo(addressWidth, doc.y);
+    doc.lineTo(addressWidth, oldY);
+    doc.stroke();
+    doc.moveTo(addressWidth, doc.y);
+    doc.lineTo(20, doc.y);
+    doc.stroke();
+    doc.moveTo(20, doc.y)
     doc.moveDown();
  } 
 
  function addProperties(doc, item, JSON) {
+    var oldY = doc.y;
+    var boxWidth = (350);
+    doc.lineWidth(1);
+    doc.lineCap('square');
+    doc.moveTo(20, doc.y);
+    doc.lineTo(boxWidth, doc.y);
+    doc.stroke();
+    doc.moveTo(20, doc.y);
       for (p in item.properties) {
       doc.text(item.properties[p].value);
       doc.moveDown(1);
@@ -126,4 +191,15 @@ function sendMail(JSON) {
     if (JSON.note) {
       doc.text("Notes: " + JSON.note);
     }
+
+    doc.moveTo(20, doc.y);
+    doc.lineTo(20, oldY);
+    doc.stroke();
+    doc.moveTo(boxWidth, doc.y);
+    doc.lineTo(boxWidth, oldY);
+    doc.stroke();
+    doc.moveTo(boxWidth, doc.y);
+    doc.lineTo(20, doc.y);
+    doc.stroke();
+    doc.moveTo(20, doc.y)
  }
